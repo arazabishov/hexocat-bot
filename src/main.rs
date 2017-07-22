c287e4c97a86919efcb1aded88243290b00bfc31
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-07-22 11:56:58 +0400

    Specified models for working with the GitHub's search endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Deserialize)]
+struct Owner {
+    login: String,
+    html_url: String
+}
+
+#[derive(Deserialize)]
+struct Repository {
+    name: String,
+    html_url: String,
+    description: String,
+    owner: Owner
+}
+
+#[derive(Deserialize)]
+struct SearchResult {
+    items: Vec<Repository>
+}
+
 fn main() {
     println!("Hello, world!");
 }

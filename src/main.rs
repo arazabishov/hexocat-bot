04d7e752429262223195b73907d060940659cdce
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-07-25 09:57:08 +0400

    Defined GitHubService for searching repositories. Initializing service within main function.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+#[macro_use]
+extern crate anterofit;
+
+use std::env;
+use anterofit::{Adapter, Url};
+use anterofit::net::intercept::AddHeader;
+use useragent::UserAgentHeader;
+
+mod useragent;
+
 #[derive(Deserialize)]
 struct Owner {
     login: String,
@@ -23,6 +33,39 @@ struct SearchResult {
     items: Vec<Repository>
 }
 
+service! {
+    trait GitHubService {
+        fn search(&self, q: String, p: u32) -> SearchResult {
+            GET("/search/repositories");
+            query!{ "q" => q, "per_page" => p }
+        }
+    }
+}
+
+fn prepare_response_body(repositories: Vec<Repository>) -> String {
+    return repositories.iter()
+        .map(|repo| format!("{0} by {1}: {2}", repo.name, repo.owner.login, repo.html_url))
+        .collect::<Vec<String>>()
+        .join("\n");
+}
+
 fn main() {
-    println!("Hello, world!");
+    if env::args().count() == 0 || env::args().count() == 1 {
+        println!("Please, specify repository name you would like to find.");
+        return;
+    }
+
+    let service = Adapter::builder()
+        .base_url(Url::parse("https://api.github.com").unwrap())
+        .interceptor(AddHeader(UserAgentHeader("hexocat-bot".to_string())))
+        .serialize_json()
+        .build();
+
+    let repository = env::args().last().unwrap();
+    let response = match service.search(repository.to_string(), 10).exec().block() {
+        Ok(result) => prepare_response_body(result.items),
+        Err(error) => "Oops, something went wrong.".to_string()
+    };
+
+    println!("{}", response);
 }
